5c6c32a2679dba3939e5cbe7d36f9d34cee281ab
irieadam <irieadam5@hotmail.com>
2016-05-26 19:32:51 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+MongoClient.connect('mongodb://localhost:27017/video', function (err,db) {
+    assert.equal(null,err);
+    console.log("Successfully connected to server");
+    
+    db.collection('movies').find({}).toArray(function (err,docs) {
+        docs.forEach(function(doc) {
+            console.log(doc.title);
+        });
+        
+        db.close();
+    });
+    console.log("Called find()");    
+});
