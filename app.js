38184c11602451fd41f6111e16ee8a5812305bc4
irieadam <irieadam5@hotmail.com>
2016-05-27 09:12:35 +0200

    week1

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    assert = require('assert'),
+    express = require('express'),
+    app = express(),
+    engines = require('consolidate');
+
+
+app.engine('html',engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 MongoClient.connect('mongodb://localhost:27017/video', function (err,db) {
     assert.equal(null,err);
     console.log("Successfully connected to server");
-    
-    db.collection('movies').find({}).toArray(function (err,docs) {
-        docs.forEach(function(doc) {
-            console.log(doc.title);
+
+    app.get('/', function (req,res) {
+        db.collection('movies').find({}).toArray(function (err,docs) {
+            res.render('movies' , { 'movies': docs})    
         });
         
-        db.close();
     });
-    console.log("Called find()");     
+
+    var server = app.listen(3000,function(){
+        var port = server.address().port;
+        console.log('Express server listening');
+    });
+    
+    app.get('/:name', function(req, res, next) {
+        var name = req.params.name;
+        var getvar1 = req.query.getvar1;
+        var getvar2 = req.query.getvar2;
+        res.render('hello', { name : name, getvar1 : getvar1, getvar2 : getvar2 });
+    });
+
+    console.log("Called find()");
 });
+
